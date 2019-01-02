c8dc8b21f53024f979422af24968afec6fc639d1
Paisa Games <41273603+paisagames@users.noreply.github.com>
2019-01-02 01:29:39 -0600

    s
    s

--- codigohelp.cs
+++ codigohelp.cs
@@ -4,3 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using MySql.Data.MySqlClient;
 using UnityEngine.SceneManagement;
+
+public class codigo_help_xd : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	Debug.Log("hola");	
+	}
+}
