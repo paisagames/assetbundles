893bca1c7ae4e09dd9752ca162ed5bf4da769f77
Paisa Games <41273603+paisagames@users.noreply.github.com>
2019-01-14 18:32:42 -0600

    php
    ccc

---
+++ php_unity_forum.php
@@ -0,0 +1,19 @@
+<?php
+
+$host_name  =   "sql9.freemysqlhosting.net";
+$database   =   "sql9270836";
+$user_name  =   "sql9270836";
+$password   =   "56751208.FtS";
+
+
+$conn   =   new mysqli($host_name,$user_name,$password,$database);
+$conn->set_charset('utf8');
+if($conn->connect_error){
+    die("Connection failed:" . $conn->connect_error);
+}
+
+$sql ="INSERT INTO usuarios_restaurantes (usuario,restaurant,correo,password,modalidad,fecha_de_registro,registrado_por) VALUES ('php','php2','php3','prueba_android','php','prueba_android','prueba_android');";
+$result =   $conn->query($sql);
+
+$conn->close();
+?>
